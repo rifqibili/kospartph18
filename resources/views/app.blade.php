83407ed612f0d0bfe55747dc2cc0451d76fc293b
Muhamad Rifqi Al Ardabili <m.rifqialardabili@gmail.com>
2026-06-23 15:02:33 +0700

    remove-unused-preload-tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,8 +20,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Preload Hero Image to improve LCP -->
-        <link rel="preload" as="image" href="/images/ruang tamu.jpg">
 
         <!-- Scripts -->
         @routes
